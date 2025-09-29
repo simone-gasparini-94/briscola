6772ef897e9a82da57d7d17255cd9a85a4f8bfb4
Simone Gasparini <simone.gasparini.2@gmail.com>
2025-09-29 19:23:51 +0200

    Start working on computer's intelligence
    Started implementing the function that handles the computer's choice
    on which card to play

--- src/computer.c
+++ src/computer.c
@@ -1,8 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "computer.h"
 #include "deck.h"
 
+static size_t	take(t_deck *deck);
+static size_t	leave(t_deck *deck);
+
 size_t	handle_computer_choice(t_deck *deck)
 {
-	(void)deck;
-	return (0);
+	size_t	i;
+
+	if (deck->first == deck->player2)
+		return (rand() % 3);
+	else if (deck->second == deck->player2)
+	{
+		if (is_points(deck->player1->played) == true)
+			i = take(deck);
+		else
+			i = leave(deck);
+	}
+	return (i);
 }
+
+static size_t	take(t_deck *deck);
+static size_t	leave(t_deck *deck);
